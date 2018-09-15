fbb80f7575ec2d87d1fe157cddaf14b33f8ffe0a
Ivan Smollet <smollet777@bigmir.net>
2018-09-15 19:46:55 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  pump = require('pump'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  concat = require('gulp-concat'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  del = require('del');
+
+gulp.task('sass', (cb) => {
+  pump([
+      gulp.src('public/sass/**/*.+(sass|scss)'),
+      sass(),
+      autoprefixer(),
+      gulp.dest('public/css'),
+      browserSync.stream()
+    ],
+    cb
+  );
+});
+
+gulp.task('browser-sync', () =>
+  browserSync.init({
+    server: "./public",
+    files: "",
+    notify: false
+  })
+);
+
+gulp.task('watch', ['browser-sync', 'sass'], () => {
+  gulp.watch(['public/sass/**/*.+(sass|scss)'], ['sass']);
+  gulp.watch(['*.html', 'public/javascripts/**/*.js']).on('change', browserSync.reload);
+});
+
+gulp.task('scripts', (cb) => {
+  pump([
+      gulp.src('public/javascripts/*.js'),
+      concat('all.min.js'),
+      babel({
+        presets: ['env']
+      }),
+      uglify(),
+      gulp.dest('public/dist/')
+    ],
+    cb
+  );
+});
+
+gulp.task('del', () => del.sync(['public/dist/**/*']));
+
+gulp.task('build', ['del', 'scripts'], (cb) => {
+  pump([
+      gulp.src('public/sass/**/*.+(sass|scss)'),
+      sass({
+        outputStyle: 'compressed'
+      }),
+      autoprefixer(),
+      gulp.dest('public/dist')
+    ],
+    cb
+  );
+});
