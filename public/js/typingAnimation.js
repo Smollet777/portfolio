e36033bf3e71a12f17f55e4c6deea7641ceb6c52
Ivan Smollet <smollet777@bigmir.net>
2018-09-19 18:46:44 +0300

    typing animation

---
+++ public/js/typingAnimation.js
@@ -0,0 +1,13 @@
+let text = document.querySelector(".typing").innerHTML.split('');
+document.querySelector('.typing').innerHTML = '';
+
+function frameLooper() {
+  if (text.length) {
+    document.querySelector(".typing").innerHTML += text.shift();
+  } else {
+    clearTimeout(0);
+  }
+  loopTimer = setTimeout('frameLooper()', 50);
+
+}
+frameLooper();
